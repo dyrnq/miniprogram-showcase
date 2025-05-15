2b84bf3cf64afee63a439007e371edd9cc9f96c5
dyrnq <dyrnq@qq.com>
2025-05-15 15:47:16 +0800

    Amend pages/form wx.request using multipart/form-data; boundary=

--- miniprogram/pages/form/form.js
+++ miniprogram/pages/form/form.js
@@ -1,12 +1,109 @@
 // pages/form.js
+const FormData = require('../../lib/wx-formdata/formData.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    selectedImage: "",
+    selectedImageName: "",
+    textVal: "",
+    response: "",
+  },
+
+handleTextInput: function(event) {
+    this.setData({
+        textVal:event.detail.value
+    })
+},
+
+  handleFormSubmit: function(event) {
+    console.log(event)
+    console.log(this.data.textVal)
+    console.log(this.data.selectedImage)
+
+    if (this.data.selectedImage ==''){
+      wx.showToast({
+        title: "please select image!",
+        icon: "none",
+        mask: true
+      })
+      return;
+    }
+
+    let formData = new FormData();
+
+
+    formData.append("name", this.data.textVal);
+    formData.appendFile("file", this.data.selectedImage, this.data.selectedImageName);
+
+    let data = formData.getData();
+    wx.request({
+      url: 'http://127.0.0.1:5588/upload-multipart',
+      method: 'POST',
+      header: {
+        'Content-Type': data.contentType
+      },
+      data: data.buffer,
+      success: (res) => {
+        console.log(res.data)
+        this.setData(
+          {
+            response: JSON.stringify(res.data)
+          }
+        )
+      },
+      fail: (res) => {
+        console.log("request fail")
+        wx.showToast({
+          title: res.errMsg,
+          icon: "none",
+          mask: true
+        })
+      },
+      complete: (res) => {
+         console.log("request complete")
+      }
+
+    })
+
 
   },
+
+  handleRemoveImg: function(event) {    
+    this.setData({
+      selectedImage: "",
+      selectedImageName: ""
+    })
+  },
+
+handleChooseImg: function (event) {
+    wx.chooseMedia({
+      count: 9,
+      sizeType:['original','compressed'],
+      sourceType:['album','camera'],
+      success:(result)=>{
+        console.log(result);
+
+        var tempFiles = result.tempFiles;
+
+        tempFiles.forEach(function(file) {
+          console.log(file.name);
+        });
+
+
+        var filePath = tempFiles[0].tempFilePath;
+        var fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+        console.log(fileName)
+        this.setData({
+          selectedImage: filePath,
+          selectedImageName: fileName
+        })
+      }
+    })
+},
+
 
   /**
    * 生命周期函数--监听页面加载
